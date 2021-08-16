e4b0fd4b6868025d3ea4aa07803e9d7211351a01
vivekkate <vivekkate999@gmail.com>
2021-08-16 15:53:22 +0530

    Integrated eureka with loan

---
+++ zulu-gateway/zulu-gateway/src/main/java/com/fis/zulugateway/ZuluGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fis.zulugateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuluGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuluGatewayApplication.class, args);
+	}
+
+}
